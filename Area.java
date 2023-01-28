64bcf242528d8315e62a37bf10ad2f4e25cb7717
saranyasai1415 <saranyasai1415@gmail.com>
2023-01-28 16:07:57 +0000

    calculates area

---
+++ Area.java
@@ -0,0 +1,13 @@
+
+public class Area {
+	
+	public static void main(String args[]) {
+		
+		float radius = 9.6f;
+		final float pie = 3.14f;
+		double area = pie * radius *radius;
+		
+		System.out.println("area of circle is :"+ String.format("%.2f", area));
+	}
+
+}
